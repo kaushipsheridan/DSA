b751f340c48391549ca31fdd0bb3ebe7e7c75e8a
Priyanshu Kaushik <kaushipr@sheridancollege.ca>
2025-06-09 16:25:34 -0400

    some more done

--- Arrays/removeDuplicatesFromSorted.java
+++ Arrays/removeDuplicatesFromSorted.java
@@ -1,8 +1,52 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class removeDuplicatesFromSorted {
 
+    //TC and SC is O(n) but we can do better
+
+    // public static int removeIt(int[] nums){
+
+    //     if(nums.length==0){return 0;}
+    //     HashSet set= new HashSet<>();
+    //     int arrindex=0;
+    //     int count=0;
+    //     for(int i=0;i<nums.length;i++){
+    //         if(set.add(nums[i])){ 
+    //             count++; 
+    //             nums[arrindex]=nums[i];
+    //             arrindex++; 
+    //         }
+    //     }
+    //     return count;
+    // }
+
+    //TC is O(n) and SC is O(1)
+    public static int removeIt(int[] nums){
+
+    if(nums.length==0){return 0;}
+    int i=1;
+    for(int j=1;j<nums.length;j++){
+        if(nums[j]!=nums[i-1]){
+            nums[i]=nums[j];
+            i++;
+        }    
+        
+    }
+    
+    return i;
+    }
+    
     public static void main(String[] args) {
-        int[] arr = {1,1,2,3,4,5};
-        int 
+        int[] nums = {1,1,2,2};
+        int rel= removeIt(nums);
+        System.out.println(rel);
+
+        System.out.println("------");
+
+        for(int hehe: nums){
+            System.out.println(hehe);
+        }
     }
     
 }
---
+++ Arrays/rotateArrayByK.java
@@ -0,0 +1,31 @@
+public class rotateArrayByK {
+
+    public static void rotateIt(int[] nums , int k){
+
+        int[] arr = nums;
+
+        int ind=0;
+        
+        for(int i=0;i<k;i++){
+
+
+
+            
+
+
+        }
+
+
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7};
+        rotateIt(arr,3);
+
+        for(int i: arr){
+            System.out.println(i);
+        }
+    }
+    
+}
