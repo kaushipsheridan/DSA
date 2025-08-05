daf4021b414fb294e231beac850cd4271d32ec78
kaushipsheridan <kaushipr@sheridancollege.ca>
2025-08-05 17:01:24 -0400

    S5L1 4 done

---
+++ Strings/largestOddNumber.java
@@ -0,0 +1,36 @@
+package Strings;
+
+public class largestOddNumber {
+
+    public static String returnOdd(String s){
+
+        int index=0;
+
+        for(int i=s.length()-1;i>=0;i--){
+            if((int)s.charAt(i)%2!=0){
+
+                index=i;
+                break;
+            }
+        }
+
+       if(index!=0){
+        return s.substring(0, index+1);
+       }else if (index==0){
+            if((int)s.charAt(index)%2!=0){
+                return  s.charAt(index)+"";
+            }
+       }
+
+
+       return "";
+
+
+    }
+ 
+    public static void main(String[] args) {
+        String s = "444";
+        System.out.println(returnOdd(s));
+    }
+    
+}
