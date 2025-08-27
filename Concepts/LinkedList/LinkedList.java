c09d36fb8edffd9652f96af0310046fc2e8ad7ea
Priyanshu Kaushik <kaushipr@sheridancollege.ca>
2025-08-26 22:42:53 -0400

    LL implementation

--- Concepts/LinkedList/LinkedList.java
+++ Concepts/LinkedList/LinkedList.java
@@ -21,19 +21,46 @@ public class LinkedList {
 
 
     }
+
+    //function to convert to LinkedList
+
+    public static Node convert2LL(int[] arr ){
+
+        //first we make head -> make it point to null( will update its pointer later)
+        Node head = new Node(arr[0]);
+
+        Node mover = head;
+
+        for(int i=1;i<arr.length;i++){
+            //add a new node (temp) , add next value of array and make it point to null
+            Node temp = new Node(arr[i]);
+            //amke sure head/prev node's next point to this newly created node
+            mover.next=temp;
+            ////update the last node 
+            mover=temp;
+
+        }
+
+        return head;
+
+    }
     
 
     public static void main(String[] args) {
 
         int[] arr = {2, 5, 8, 7};
+ 
+        // // Creating a new Node with the value from the array
+        // Node x = new Node(arr[0],null);
+
+        // // Printing location of x and the data stored in the Node x
+        // System.out.println(x);
+        // System.out.println(x.data);
+
+        Node head = convert2LL(arr);
+        System.out.println(head.data);
 
 
-        // Creating a new Node with the value from the array
-        Node x = new Node(arr[0],null);
-
-        // Printing location of x and the data stored in the Node x
-        System.out.println(x);
-        System.out.println(x.data);
         
 
     }
---
+++ Concepts/LinkedList/insertionAndDeletionLL.java
@@ -0,0 +1,9 @@
+package Concepts.LinkedList;
+
+public class insertionAndDeletionLL {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
